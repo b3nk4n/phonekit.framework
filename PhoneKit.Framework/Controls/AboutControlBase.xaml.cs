235f3cb9fcf171caaaf45880ec1331cba00811f0
bsautermeister <apps@bsautermeister.de>
2014-02-14 00:53:52 +0100

    Removed dynamic framework version via assambly in the about page, because it was not reliable.

--- PhoneKit.Framework/Controls/AboutControlBase.xaml.cs
+++ PhoneKit.Framework/Controls/AboutControlBase.xaml.cs
@@ -42,7 +42,6 @@ namespace PhoneKit.Framework.Controls
         {
             InitializeComponent();
             LocalizeContent();
-            SetFrameworkBranding();
 
             // Note: register events in code and not in XAML because this
             //       is not allowed when inheritance is used
@@ -89,18 +88,6 @@ namespace PhoneKit.Framework.Controls
         /// Localizes the user control content and texts.
         /// </summary>
         protected abstract void LocalizeContent();
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Sets the framework branding.
-        /// </summary>
-        private void SetFrameworkBranding()
-        {
-            BrandingElement.Text = "powered by PhoneKit Framework " + VersionHelper.GetFrameworkVersionText();
-        }
 
         #endregion
 
