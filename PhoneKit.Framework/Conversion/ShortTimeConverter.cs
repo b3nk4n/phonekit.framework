39e586737b7e8097678f840c6ae0b58f9cb1bba9
bsautermeister <apps@bsautermeister.de>
2014-02-26 14:37:00 +0100

    Added short time converter.

---
+++ PhoneKit.Framework/Conversion/ShortTimeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PhoneKit.Framework.Conversion
+{
+    /// <summary>
+    /// Converter to convert a DateTime object into a short date format.
+    /// </summary>
+    public class ShortTimeConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts the object to a string.
+        /// </summary>
+        /// <param name="value">The datetime value.</param>
+        /// <param name="targetType">The target conversion type.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="culture">The culture information.</param>
+        /// <returns>The short time.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime)
+                return ((DateTime)value).ToShortTimeString();
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Backwards conversion is not supported.
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
