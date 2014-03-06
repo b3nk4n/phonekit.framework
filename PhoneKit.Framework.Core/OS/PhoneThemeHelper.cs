0bdcb9ce79fe474fe7ca05a3202281dc10dd73f0
bsautermeister <apps@bsautermeister.de>
2014-03-06 10:34:34 +0100

    Added phone theme helper for theme color management.

---
+++ PhoneKit.Framework.Core/OS/PhoneThemeHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace PhoneKit.Framework.Core.OS
+{
+    /// <summary>
+    /// Helper class for phone theme management.
+    /// </summary>
+    public static class PhoneThemeHelper
+    {
+        /// <summary>
+        /// Indicates whether the dark theme is active.
+        /// </summary>
+        /// <returns>Returns true if the dark theme is active.</returns>
+        public static bool IsDarkThemeActive
+        {
+            get
+            {
+                Visibility darkBackgroundVisibility =
+                (Visibility)Application.Current.Resources["PhoneDarkThemeVisibility"];
+
+                return darkBackgroundVisibility == Visibility.Visible;
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether the light theme is active.
+        /// </summary>
+        /// <returns>Returns true if the dark theme is active.</returns>
+        public static bool IsLightThemeActive
+        {
+            get
+            {
+                return !IsDarkThemeActive;
+            }
+        }
+    }
+}
