a3a1c30c240d9fee3455c79ce51fcaa804204063
bsautermeister <apps@bsautermeister.de>
2014-03-16 19:13:34 +0100

    Added vibration helper.

---
+++ PhoneKit.Framework/OS/VibrationHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneKit.Framework.OS
+{
+    /// <summary>
+    /// The vibration helper class.
+    /// </summary>
+    public static class VibrationHelper
+    {
+        /// <summary>
+        /// The vibration controller.
+        /// </summary>
+        private static VibrateController _vibrateController = VibrateController.Default;
+
+        /// <summary>
+        /// Starts to vibrate.
+        /// </summary>
+        /// <param name="seconds">The time span in seconds.</param>
+        public static void Vibrate(double seconds)
+        {
+            _vibrateController.Start(TimeSpan.FromSeconds(seconds));
+        }
+
+        /// <summary>
+        /// Stops to vibrate.
+        /// </summary>
+        public static void Stop()
+        {
+            _vibrateController.Stop();
+        }
+    }
+}
